6449d9816d5aabaefe9f466b2287c91b67dad688
Azka <azkadaiki26@gmail.com>
2026-06-20 21:50:16 +0700

    feat: github profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+function ProfilePicture({ userId }: { userId: string }) {
+  return (
+    <img
+      src={"https://avatars.githubusercontent.com/u/" + userId}
+      alt="GitHub Profile"
+    />
+  );
+}
+
+function ProfileLink({ username }: { username: string }) {
+  return <a href={"https://github.com/" + username}>{username}</a>;
+}
+
+function GithubInfo({
+  username,
+  userId,
+}: {
+  username: string;
+  userId: string;
+}) {
+  return (
+    <div className="github-info">
+      <ProfilePicture userId={userId} />
+      <ProfileLink username={username} />
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div className="flex justify-center">
-      <p className="text-lg">This Is Content of paragraph</p>
+    <div className="flex justify-center ">
+      <GithubInfo username="daffaazka" userId="25724809" />
     </div>
   );
 }
